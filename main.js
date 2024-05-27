dbf9476db0a45b559402351d1ed11b711fe3b719
sult <168359467+Salbdr@users.noreply.github.com>
2024-05-27 18:10:53 +0300

    lap1-Dom

---
+++ main.js
@@ -0,0 +1,106 @@
+// function clicMe(){            ////للبوتن
+//     alert clicMe("clic me")
+// }
+
+
+//// الحين نبي نسوي اذا ضغطنا البوتن يعطينا اسم ثاني
+
+
+
+//////////////////lap1////////////////
+
+
+
+let userName= document.getElementById("name")
+console.log(userName);
+
+var button1= document.getElementById("button1")
+
+button1.addEventListener("click", ()=> {
+
+
+    userName.innerHTML="tuwaiq"    
+       
+    
+    
+    
+    
+    
+    }
+    )
+    
+
+
+
+
+
+
+    
+    var button2= document.getElementById("button2")
+    
+    button2.addEventListener("click", ()=> {
+    
+    
+        userName.style.fontSize="large"    
+           
+        
+        
+        
+        
+        
+        }
+        )
+
+
+        var button3= document.getElementById("button3")
+    
+        button3.addEventListener("click", ()=> {
+        
+        
+            userName.style.fontSize="small"    
+               
+            
+            
+            
+            
+            
+            }
+            )
+    
+
+
+
+            var button4= document.getElementById("button4")
+    
+            button4.addEventListener("click", ()=> {
+            
+            
+                userName.style.color="blue"
+                   
+                
+                
+                
+                
+                
+                }
+                )
+
+
+
+                var button5= document.getElementById("button5")
+    
+                button5.addEventListener("click", ()=> {
+                
+                
+                    userName.style.backgroundColor="green"
+                       
+                    
+                    
+                    
+                    
+                    
+                    }
+                    )
+
+
+
